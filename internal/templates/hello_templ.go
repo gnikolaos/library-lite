057c04e89751bd533ebff3bd6d19f1f66bbcf920
dizars1776 <dizars1776@yahoo.gr>
2024-04-20 20:03:06 +0300

    fix: add acme font + footer

--- internal/templates/hello_templ.go
+++ internal/templates/hello_templ.go
@@ -30,7 +30,7 @@ func Hello(name string) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/hello.templ`, Line: 5, Col: 17}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/hello.templ`, Line: 5, Col: 15}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["internal/templates/**/*.templ"],
     theme: {
         extend: {
+            fontFamily: {
+                acme: ["acme", "sans-serif"],
+            },
             colors: {
                 primary: "#d3cabb", // Light beige
                 accent: "#a39171", // Light brown
